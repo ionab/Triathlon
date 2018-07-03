e23b07a1787f90c8cc3ecd603bca0638f5d9d9b9
Kelsie Braidwood <kelsieiona@gmail.com>
2018-07-03 13:27:58 +0100

    updates css

--- client/src/components/Favourite.js
+++ client/src/components/Favourite.js
@@ -1,11 +1,11 @@
 import React from "react";
+import './favourite.css'
 
 const Favourite = function(props){
 
-
   return(
-    <div>
-      <h4>{props.favourite.event_title}</h4>
+    <div className="favourite">
+      <h4 className="title">{props.favourite.event_title}</h4>
       <p>{props.favourite.event_date}</p>
       <p>Country: {props.favourite.event_country}</p>
       <p>Venue: {props.favourite.event_venue}</p>
