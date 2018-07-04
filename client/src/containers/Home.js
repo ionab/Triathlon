b1e58213de43bacf8356a5bdae2dbb3239c70fd0
Kelsie Braidwood <kelsieiona@gmail.com>
2018-07-04 10:15:13 +0100

    home page css

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -3,11 +3,10 @@ import './home.css'
 
 const Home = (props) =>(
   <body className="body">
-    <h1>Welcome to iTri</h1>
-    <p className="para">Here you can find out about triathlon events all around europe in 2018.<br/>
+    <h1 className="header">iTri</h1>
+    <p className="paragraph">Here you can find out about triathlon events all around europe in 2018.<br/>
     Add a favourite on the events page and view the details on the favourites page.
-
-  </p>
+    </p>
   </body>
 );
 export default Home;
